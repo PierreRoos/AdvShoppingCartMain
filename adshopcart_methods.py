d95cb9c673f7a1aae2802d89021d9eb7afa8649a
Pierre Roos <101295194+PierreRoos@users.noreply.github.com>
2022-03-24 16:37:03 -0700

    update shopping cart scenario

--- adshopcart_methods.py
+++ adshopcart_methods.py
@@ -7,14 +7,15 @@ import adshopcart_locators as locators
 s = Service(executable_path='../chromedriver.exe')
 driver = webdriver.Chrome(service=s)
 
+
 def setUp():
-    print(f'--------------------------------------- SET UP FUNCTION --------------------------------------------')
-    print(f'The test is started at: {datetime.datetime.now()}')
+    print(f'----- SET UP FUNCTION --------------------------------------------')
+    print(f'The test started at: {datetime.datetime.now()}')
     driver.maximize_window()
     driver.implicitly_wait(30)
     driver.get(locators.hmpg_url)
-    if driver.current_url == locators.hmpg_url and driver.title == locators.hmpg_title:
-        print(f'{locators.app}  launched successfully.')
+    if driver.current_url == locators.hmpg_url:  # and driver.title == locators.hmpg_title:  # title doesn't work
+        print(f'{locators.app} launched successfully.')
         print(f'{locators.app} homepage url:{driver.current_url}, title: {driver.title}')
         sleep(0.25)
     else:
@@ -25,8 +26,12 @@ def setUp():
 
 def tearDown():
     if driver is not None:
-        print('------------------------------- TEAR DOWN FUNCTION ----------------------------------------------------')
+        print('----- TEAR DOWN FUNCTION ----------------------------------------------------')
         print(f'The test is completed at: {datetime.datetime.now()}')
         sleep(2)
         driver.close()
         driver.quit()
+
+
+setUp()
+tearDown()
